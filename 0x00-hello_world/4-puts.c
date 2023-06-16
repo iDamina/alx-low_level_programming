7d7a04a49da9add90683e08fa2468074e39bf8ff
iDamina <daminailiya@gmail.com>
2023-06-16 02:02:46 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#include<stdio.h>
+i#include<stdio.h>
+/**
+ * main - Prints programming is like building.
+ * return: always 0 to give success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+/**
+  * return - this returns value 0 as success
+  */
+	return (0);
 }
