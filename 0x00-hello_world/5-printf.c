a08b3607e3042987e6832eea050e37233a746971
iDamina <daminailiya@gmail.com>
2023-06-16 02:46:03 +0100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  *main - program to print output using the printf call function
+  *Return: returns 0 as a success
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
