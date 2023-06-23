25b131c87271d7f7e43aabc0e6b599c8702a9547
iDamina <daminailiya@gmail.com>
2023-06-23 21:48:41 +0100

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_isupper - program to determine is an alphabet is upper starts
+  *
+  *@c: integer parameter in our function
+  *
+  *Return: returns 1 if c is upper and 0 otherwise
+  */
+int _isupper(int c)
+{
+	if (c <= 65 && c <= 90)
+	{
+		return (1);
+	}
+		return (0);
+}
