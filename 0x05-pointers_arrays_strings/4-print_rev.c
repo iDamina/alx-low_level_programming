1084126788a3c0e0a8d365a52fcc5da4565442c9
iDamina <daminailiya@gmail.com>
2023-06-27 00:48:36 +0100

    print reversal

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,10 @@ void print_rev(char *s)
 	int a = 0;
 
 	while (s[a] != '\0')
-	{
 		a++;
-			for (a = a - 1; a >= 0; a--)
-			{
-				_putchar(s[a]);
-			}
-		_putchar('\n');
-	}
+
+	for (a = a - 1; a >= 0; a--)
+		_putchar(s[a]);
+
+	_putchar('\n');
 }
