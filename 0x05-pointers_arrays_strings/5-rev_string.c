a7467ddd19d28a68650c5516b1b74843aec54af8
iDamina <daminailiya@gmail.com>
2023-06-27 01:33:12 +0100

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *rev_string - reverses string
+  *@s: string
+  *Return: returns void
+  */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rev = s[i];
+		s[i] = s[c];
+		s[c] = rev;
+	}
+}
