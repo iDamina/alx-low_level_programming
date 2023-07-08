2ad41fbcef91f341cde3470d7d27be9c20b46c9f
iDamina <daminailiya@gmail.com>
2023-07-09 00:19:23 +0100

    static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 /**
   *_puts - start of the program
   *@s: input character
   */
 void _puts(char *s)
 {
-	_putchar('s');
+	int i = 0;
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
 }
