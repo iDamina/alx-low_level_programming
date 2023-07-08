a3aa1c4cefbe1a5ae6c02d80c43cf1e1736eb260
iDamina <daminailiya@gmail.com>
2023-07-08 21:05:40 +0100

    creating a static library

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include<stdio.h>
+/**
+  *_puts - Entry point
+  *@s: character s
+  * Return: Always 0 (Success)
+  */
+void _puts(char *s)
+{
+	puts(s);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+Here, we are working on static libraries. How to create and use it
