57d872d034492e038162afbc7dfce82927e36cd4
iDamina <daminailiya@gmail.com>
2023-07-18 23:49:55 +0100

    dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+  *struct dog - dog class
+  *
+  *@name: Name of the dog
+  *
+  *@age: age of the dog
+  *
+  *@owner: owner of the dog
+  *
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
