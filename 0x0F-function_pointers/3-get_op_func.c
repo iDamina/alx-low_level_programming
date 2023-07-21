66d9882d94c42080206eb16b60c50acbba6d4d6d
iDamina <daminailiya@gmail.com>
2023-07-21 23:26:22 +0100

    calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
   * get_op_func - select the correct operation function asked by user
@@ -22,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (strcmp((s, ops[i].op == 0)
+		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
 		i++;
 	}
