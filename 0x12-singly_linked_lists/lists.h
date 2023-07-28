bf9c885bd9a20325e70da9adde1cef153ff74ee4
iDamina <daminailiya@gmail.com>
2023-07-28 17:43:51 +0100

    list header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,18 @@
 #ifndef LISTS_H
 #define LISTS_H
+/**
+  *struct list_s - linked list
+  *@str: malloc string
+  *@len: length of the string
+  *@next: points to the next node
+  *Description: singly list structure
+  */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
