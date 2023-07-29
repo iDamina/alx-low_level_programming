613db2d40d2b4914845f768a3ade185c10b80062
iDamina <daminailiya@gmail.com>
2023-07-29 02:58:39 +0100

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+extern printf;
+
+section:data; Data section, initialized variables
+	msg: db "Hello, Holberton\n", 0;
+	format: db "%s", 10, 0; The printf format, "\n", '0'
+
+section:text; code section here
+	global main; the standard gcc entry point
+	main:;
+	push rbp;
+
+	mov rdi, format
+	mov rsi, msg
+	mov rax,0;
+	call printf;
+
+	pop rbp; restore stack
+
+	mov rax,0;
+	ret;
