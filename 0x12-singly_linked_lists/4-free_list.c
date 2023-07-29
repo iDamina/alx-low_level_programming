75a72a47dc0bc1d52cc552ca4934716433b35056
iDamina <daminailiya@gmail.com>
2023-07-29 01:48:00 +0100

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  *free_list - to free list
+  *@head: head as a list to free
+  */
+void free_list(list_t *head)
+{
+	list_t *temporary;
+
+	while (head)
+	{
+		temporary = head->next;
+		free(head->str);
+		free(head);
+		head = temporary;
+	}
+}
