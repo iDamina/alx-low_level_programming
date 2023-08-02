345468855d9b73dd5264e23a3a2e6be6742e703e
iDamina <daminailiya@gmail.com>
2023-08-02 11:49:54 +0100

    free listint2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,10 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
 	}
 
-	*head = NULL;
+	head = NULL;
 }
