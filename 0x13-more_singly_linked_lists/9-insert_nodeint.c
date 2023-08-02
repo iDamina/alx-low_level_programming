dff3e99172ad50b1ee2fe13bb52950b98bc752bc
iDamina <daminailiya@gmail.com>
2023-08-02 01:32:00 +0100

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,13 +15,15 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int node;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
+
 	if (idx == 0)
 	{
-		new->next = copy;
+		new->next = head;
 		*head = new;
 		return (new);
 	}
