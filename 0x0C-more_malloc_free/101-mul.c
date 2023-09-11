9b554a6e6abe9977da5019e801c08dd815e1580c
iDamina <daminailiya@gmail.com>
2023-09-11 16:10:19 +0100

    multiplication in base 10

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: n no of arguments
+ * @argv: arguments
+ * Return: integer
+ **/
+
+int main(int argc, char *argv[])
+{
+	unsigned long mul;
+	int i, j;
+
+	/* checking condition to print Error n an exit with 98 */
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	/* another check */
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > 57 || argv[i][j] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	/* adding the second and third value after mul */
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+	return (0);
+}
