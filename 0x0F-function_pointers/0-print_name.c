baae77d792698f54393d64cbaf0907802f1220d1
iDamina <daminailiya@gmail.com>
2023-09-14 00:27:00 +0100

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ *print_name - a function that prints name
+ *@name: the given name
+ *@f: function of name
+ *Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
