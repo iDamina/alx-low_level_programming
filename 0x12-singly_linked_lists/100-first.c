7ebffe64f592c0154a22df9525b61fadb45b202d
iDamina <daminailiya@gmail.com>
2023-09-21 23:55:25 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *print_statement - prints a sentence before the main
+ *function is executed
+ */
+void print_statement(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
