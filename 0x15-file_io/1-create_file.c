2452fee189386ab706960fe396f731a461c4ac20
iDamina <daminailiya@gmail.com>
2023-10-04 02:17:57 +0100

    create file update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (file == -1)
 		return (-1); /* return -1 for failure*/
 	/*check and write text content to the file */
-	if (text_content)
+	if (text_content != NULL)
 	{
 		while (text_content[counter])
 			counter++;
