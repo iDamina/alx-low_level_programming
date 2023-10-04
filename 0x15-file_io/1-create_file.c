570ebc6f95f0cfd061a66c57ced42ccb66a1e116
iDamina <daminailiya@gmail.com>
2023-10-04 02:23:36 +0100

    updated create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include "main.h"
 /**
   *create_file - function that creates file
   *
@@ -13,6 +13,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 
 	/*open the file for writing */
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
