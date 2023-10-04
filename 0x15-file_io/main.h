90ddaab21977d51c0418f4333098d31ca1444a03
iDamina <daminailiya@gmail.com>
2023-10-04 02:10:50 +0100

    corrected header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,13 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _FILE_IO
+#define _FILE_IO
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
